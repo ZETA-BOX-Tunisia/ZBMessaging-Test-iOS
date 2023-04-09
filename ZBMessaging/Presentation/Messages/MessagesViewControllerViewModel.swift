83fa989eca4ae22195c05d8d3ac1b03f592c5f92
ADMIN <ADMIN@MacBook-Pro-de-SLIM.local>
2023-04-09 11:13:06 +0100

    finish the implentation of MessagesViewModel

--- ZBMessaging/Presentation/Messages/MessagesViewControllerViewModel.swift
+++ ZBMessaging/Presentation/Messages/MessagesViewControllerViewModel.swift
@@ -6,13 +6,51 @@
 //
 
 import Foundation
+import Combine
+import ZBFramework
 
-class MessagesViewControllerViewModel {
+protocol MessagesViewmodelInput {
+    func excuteAction()
+}
+
+protocol MessagesViewmodelOutput {
+    var onFailure: Published<String>.Publisher {get}
+    var onSuccess: Published<[ZBFramework.UsersPermissionsUser]>.Publisher {get}
+}
+
+protocol MessagesViewModelProtocol: MessagesViewmodelInput, MessagesViewmodelOutput {}
+
+
+
+class MessagesViewControllerViewModel: MessagesViewModelProtocol {
+   
+    @Published var wrappedcontactlist = [ZBFramework.UsersPermissionsUser]()
+    
+    @Published var wrappedOnFailure: String = String()
+    
+    var onSuccess: Published<[ZBFramework.UsersPermissionsUser]>.Publisher { $wrappedcontactlist}
+    
+    var onFailure: Published<String>.Publisher { $wrappedOnFailure }
+    
     
     private var messagesUseCase: MessagesUseCaseProtocol
     
+    private var subscriptions = Set<AnyCancellable>()
+    
     init(messagesUseCase: MessagesUseCaseProtocol) {
         self.messagesUseCase = messagesUseCase
+        self.observeResponse()
+    }
+    
+    private func observeResponse() {
+        subscriptions = [onSuccessObservation()]
+    }
+    
+    private func onSuccessObservation() -> AnyCancellable {
+        return messagesUseCase.contactList.sink { [weak self] conacts in
+            guard let StrongSelf = self else { return }
+            StrongSelf.wrappedcontactlist = conacts
+        }
     }
     
     
